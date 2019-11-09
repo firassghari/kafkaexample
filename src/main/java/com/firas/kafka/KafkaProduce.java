e7bb9a4384307e7511676af80497421dc0d1132b
firas sghar <firas.sghari@yahoo.fr>
2019-11-09 01:58:14 +0100

    proudcer with call back

--- src/main/java/com/firas/kafka/KafkaProduce.java
+++ src/main/java/com/firas/kafka/KafkaProduce.java
@@ -1,14 +1,15 @@
 package com.firas.kafka;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class KafkaProduce {
     public static void main(String[] args) {
+        final Logger logger = LoggerFactory.getLogger(KafkaProduce.class);
         Properties prop = new Properties();
         final String bootstrapServer = "127.0.0.1:9092";
         prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
@@ -16,10 +17,21 @@ public class KafkaProduce {
         prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
         KafkaProducer<String, String> producer = new KafkaProducer<String ,String>(prop);
         ProducerRecord<String,String > record = new ProducerRecord<String, String>("firas","hello world ");
-        producer.send(record);
+        producer.send(record, new Callback() {
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                if (e ==null)
+                {
+                    logger.info("message reçu dans le topic"+recordMetadata.topic());
+                }
+                else
+                {
+                    logger.error("problème lors de l'envoie du message",e);
+                }
+            }
+        });
         producer.flush();
         producer.close();
 
-    
+
     }
 }
